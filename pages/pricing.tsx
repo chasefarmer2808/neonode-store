8cd2140aec0a727849ff721cb143b2de6dafb06e
chasefarmer2808 <chasefarmer2808@ufl.edu>
2021-05-31 18:57:36 -0400

    pricing grid

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
 import { PriceOption } from "../components/PriceOption";
+import styles from "../components/pricing.module.css";
 
 export default function Pricing() {
     return (
@@ -8,12 +9,20 @@ export default function Pricing() {
             <NavBar />
             <main>
                 <h1>See which option is right for you</h1>
-                <ul>
-                    <li>
+                <ul className={styles.priceOptionList}>
+                    <li className={styles.priceOptionItem}>
                         <PriceOption title="Just the Board" price={12}/>
+                    </li>
+                    <li className={styles.priceOptionItem}>
                         <PriceOption title="Boad with Connectors" price={20} />
+                    </li>
+                    <li className={styles.priceOptionItem}>
                         <PriceOption title="Complete Board" price={40} />
+                    </li>
+                    <li className={styles.priceOptionItem}>
                         <PriceOption title="Complete Board + 1m LEDs" price={50}/>
+                    </li>
+                    <li className={styles.priceOptionItem}>
                         <PriceOption title="Everything + 1m LEDs" price={60} />
                     </li>
                 </ul>
